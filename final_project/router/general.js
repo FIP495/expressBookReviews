c36c65970aa4bbe8f885818fd96ac3e6f3b1e355
FIP <f@a.b>
2025-05-10 17:59:15 -0400

    project completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -58,5 +58,68 @@ public_users.get('/review/:isbn',function (req, res) {
   return res.status(300).json(books[isbn]["reviews"]);
 });
 
+// TASK 10 - Get all book details  using Promises
+public_users.get('/books',function (req, res) {
+    const get_books = new Promise((resolve, reject) => {
+        resolve(res.send(books));
+    });
+    get_books.
+        then(() => {console.log("Promise for Task 10 is resolved");});
+});
+
+// TASK 11 - Get book details based on ISBN using Promises
+public_users.get('/books/isbn/:isbn',function (req, res) {
+    const get_books_isbn = new Promise((resolve, reject) => {
+        const isbn = req.params.isbn;
+            if (req.params.isbn <= 10) {
+            resolve(res.send(books[isbn]));
+        } else {
+            reject(res.send('ISBN not found'));
+        }
+    });
+    get_books_isbn.
+        then(function(){
+            console.log("Promise for Task 11 is resolved");
+    }).
+        catch(() => {console.log('ISBN not found');
+    });
+
+});
+
+// TASK 12 - Get book details based on author using Promises
+public_users.get('/books/author/:author',function (req, res) {
+    const get_books_author = new Promise((resolve, reject) => {
+        const author = req.params.author;
+        let books_by_author = Object.values(books).filter((book) => {return book["author"] == author;});
+        if (books_by_author) {
+            resolve(res.send(books_by_author));
+        } else {
+           reject(res.send('author not found'));
+        }
+    });
+    get_books_author.
+        then(() => {console.log("Promise for Task 12 is resolved");
+    }).
+    catch(() =>{console.log('author not found');});
+
+});
+
+// TASK 13 - Get book details based on title using Promises
+public_users.get('/books/title/:title',function (req, res) {
+    const get_title_details = new Promise((resolve, reject) => {
+        const title = req.params.title;
+        let title_details = Object.values(books).filter((book) => {return book["title"] == title;});
+        if (title_details) {
+            resolve(res.send(title_details));
+        } else {
+           reject(res.send('title not found'));
+        }
+    });
+    get_title_details.
+        then(() => {console.log("Promise for Task 13 is resolved");
+    }).
+    catch(() => {console.log('title not found');});
+
+});
 
 module.exports.general = public_users;
