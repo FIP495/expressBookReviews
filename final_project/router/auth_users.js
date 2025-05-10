cbfd9d81a80f3544f51f29327f873b1f25304dfd
FIP <f@a.b>
2025-05-10 13:20:13 -0400

    authentication

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -13,6 +13,31 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
 }
 
+
+app.use(session({secret:"finalproject"},resave=true,saveUninitialized=true));
+
+app.use(express.json());
+
+// Middleware to authenticate requests to "/friends" endpoint
+app.use("/customer/auth/*", function auth(req, res, next) {
+    // Check if user is logged in and has valid access token
+    if (req.session.authorization) {
+        let token = req.session.authorization['accessToken'];
+
+        // Verify JWT token
+        jwt.verify(token, "access", (err, user) => {
+            if (!err) {
+                req.user = user;
+                next(); // Proceed to the next middleware
+            } else {
+                return res.status(403).json({ message: "User not authenticated" });
+            }
+        });
+    } else {
+        return res.status(403).json({ message: "User not logged in" });
+    }
+});
+
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
